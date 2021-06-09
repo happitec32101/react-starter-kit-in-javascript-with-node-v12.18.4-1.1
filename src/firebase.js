bf9fcf65c4e735a7f18edca695f74f9a6aeb6efb
happitec32101 <84065641+happitec32101@users.noreply.github.com>
2021-06-09 12:03:57 +0900

    create app

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcgvnJTY6q81ecnGQeQPzvvRraQEwPI1g",
+  authDomain: "idobatakaigi-with-ham-a9678.firebaseapp.com",
+  projectId: "idobatakaigi-with-ham-a9678",
+  storageBucket: "idobatakaigi-with-ham-a9678.appspot.com",
+  messagingSenderId: "897798987526",
+  appId: "1:897798987526:web:e4c1eac720601937014887"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+  messageRef.push({ name, text });
+};
